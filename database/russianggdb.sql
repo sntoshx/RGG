fd4496a4f20e501c02b3aae252865d23e020d391
yednaphsotnas <yednaphsotnas@gmail.com>
2019-12-04 10:02:12 +0530

    modified the database to remove an extra entity from entity set Customer

--- database/russianggdb.sql
+++ database/russianggdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 24, 2019 at 06:29 PM
+-- Generation Time: Dec 04, 2019 at 04:29 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.5
 
@@ -61,11 +61,10 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `Gender` varchar(6) NOT NULL,
   `email` varchar(255) NOT NULL,
   `City` varchar(64) NOT NULL,
-  `Pin` int(6) NOT NULL,
   `CPassword` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=216 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=226 DEFAULT CHARSET=latin1;
 
 --
 -- Triggers `customer`
@@ -130,7 +129,7 @@ CREATE TABLE IF NOT EXISTS `log` (
   `Name` varchar(255) DEFAULT NULL,
   `CreationTime` timestamp NOT NULL,
   PRIMARY KEY (`User_Number`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -142,7 +141,7 @@ DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
   `Prod_ID` int(3) NOT NULL,
   `Prod_Name` varchar(50) NOT NULL,
-  `Prod_Price` decimal(5,0) NOT NULL,
+  `Prod_Price` decimal(5,2) NOT NULL,
   PRIMARY KEY (`Prod_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -151,51 +150,51 @@ CREATE TABLE IF NOT EXISTS `products` (
 --
 
 INSERT INTO `products` (`Prod_ID`, `Prod_Name`, `Prod_Price`) VALUES
-(11, 'Apple', '100'),
-(12, 'Avacado', '299'),
-(13, 'Banana', '30'),
-(14, 'Black grapes', '110'),
-(15, 'Cherries', '240'),
-(16, 'Custard-apple', '149'),
-(17, 'Grapes', '65'),
-(18, 'Kiwi', '249'),
-(19, 'Litchi', '60'),
-(20, 'Mango', '60'),
-(21, 'Mousambi', '55'),
-(22, 'Muskmelon', '50'),
-(23, 'Orange', '35'),
-(24, 'Papaya', '45'),
-(25, 'Pears', '150'),
-(26, 'Pineapple', '55'),
-(27, 'Pomegranate', '90'),
-(28, 'Sapotta', '100'),
-(29, 'Strawberry', '30'),
-(30, 'Watermelon', '30'),
-(101, 'Aubergine', '55'),
-(102, 'Beans', '20'),
-(103, 'Beetroot', '40'),
-(104, 'Bottlegourd', '30'),
-(105, 'Broccoli', '120'),
-(106, 'Cabbage', '35'),
-(107, 'Capsicum', '60'),
-(108, 'Carrot', '70'),
-(109, 'Cauliflower', '55'),
-(110, 'Coconut', '60'),
-(111, 'Corn', '20'),
-(112, 'Cucumber', '30'),
-(113, 'Curryleaves', '200'),
-(114, 'Drumsticks', '20'),
-(115, 'Garlic', '120'),
-(116, 'Ginger', '75'),
-(117, 'Jackfruit', '175'),
-(118, 'Lemon', '100'),
-(119, 'Lettuce', '200'),
-(120, 'Mushroom', '120'),
-(121, 'Okra', '45'),
-(122, 'Onion', '50'),
-(123, 'Peas', '120'),
-(124, 'Potato', '30'),
-(125, 'Pumpkin', '40');
+(11, 'Apple', '100.00'),
+(12, 'Avacado', '299.00'),
+(13, 'Banana', '30.00'),
+(14, 'Black grapes', '110.00'),
+(15, 'Cherries', '240.00'),
+(16, 'Custard-apple', '149.00'),
+(17, 'Grapes', '65.00'),
+(18, 'Kiwi', '249.00'),
+(19, 'Litchi', '60.00'),
+(20, 'Mango', '60.00'),
+(21, 'Mousambi', '55.00'),
+(22, 'Muskmelon', '50.00'),
+(23, 'Orange', '35.00'),
+(24, 'Papaya', '45.00'),
+(25, 'Pears', '150.00'),
+(26, 'Pineapple', '55.00'),
+(27, 'Pomegranate', '90.00'),
+(28, 'Sapotta', '100.00'),
+(29, 'Strawberry', '30.00'),
+(30, 'Watermelon', '30.00'),
+(101, 'Aubergine', '55.00'),
+(102, 'Beans', '20.00'),
+(103, 'Beetroot', '40.00'),
+(104, 'Bottlegourd', '30.00'),
+(105, 'Broccoli', '120.00'),
+(106, 'Cabbage', '35.00'),
+(107, 'Capsicum', '60.00'),
+(108, 'Carrot', '70.00'),
+(109, 'Cauliflower', '55.00'),
+(110, 'Coconut', '60.00'),
+(111, 'Corn', '20.00'),
+(112, 'Cucumber', '30.00'),
+(113, 'Curryleaves', '200.00'),
+(114, 'Drumsticks', '20.00'),
+(115, 'Garlic', '120.00'),
+(116, 'Ginger', '75.00'),
+(117, 'Jackfruit', '175.00'),
+(118, 'Lemon', '100.00'),
+(119, 'Lettuce', '200.00'),
+(120, 'Mushroom', '120.00'),
+(121, 'Okra', '45.00'),
+(122, 'Onion', '50.00'),
+(123, 'Peas', '120.00'),
+(124, 'Potato', '30.00'),
+(125, 'Pumpkin', '40.00');
 
 -- --------------------------------------------------------
 
